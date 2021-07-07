aded6b848a71e834d39404e9ed13c0de1cf8396e
wujun728 <wujun728@hotmail.com>
2021-07-07 20:51:32 +0800

    1

---
+++ jun_springboot_apiservice/src/main/java/com/jun/plugin/biz/shiro/CustomHashedCredentialsMatcher.java
@@ -0,0 +1,30 @@
+package com.jun.plugin.biz.shiro;
+
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jun.plugin.biz.exception.BusinessException;
+import com.jun.plugin.biz.service.RedisService;
+
+/**
+ * @ClassName: CustomHashedCredentialsMatcher
+ * TODO:类文件简单描述
+ * @Author: 小霍
+ * @UpdateUser: 小霍
+ * @Version: 0.0.1
+ */
+public class CustomHashedCredentialsMatcher extends HashedCredentialsMatcher {
+    @Autowired
+    private RedisService redisService;
+    @Override
+    public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
+        CustomPasswordToken customPasswordToken= (CustomPasswordToken) token;
+        String sessionId= (String) customPasswordToken.getPrincipal();
+        if(!redisService.hasKey(sessionId)){
+            throw new BusinessException(4010001,"用户授权信息无效请重新登录");
+        }
+        return true;
+    }
+}
