d6327035d5e3f99b559d0f2cb7ebac420fbd3646
俊哥 <wujun728@163.com>
2021-08-14 20:15:36 +0800

    1

--- jun_springboot_api/src/main/java/com/jun/plugin/ApiServiceApplication.java
+++ jun_springboot_api/src/main/java/com/jun/plugin/ApiServiceApplication.java
@@ -1,10 +1,13 @@
 package com.jun.plugin;
 
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tk.mybatis.spring.annotation.MapperScan;
+
 @SpringBootApplication
-//@MapperScan("com.jun.plugin.api.mapper")
+@MapperScan(basePackages = {"com.jun.plugin.base.mapper","com.jun.plugin.biz.mapper"})
 public class ApiServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiServiceApplication.class, args);
--- jun_springboot_api/src/main/java/com/jun/plugin/biz/mapper/CityMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jun.plugin.biz.mapper;
-
-import com.jun.plugin.base.core.Mapper;
-import com.jun.plugin.biz.model.City;
-
-public interface CityMapper extends Mapper<City> {
-}
