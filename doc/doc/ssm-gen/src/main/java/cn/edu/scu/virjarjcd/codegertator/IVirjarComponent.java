a4ab337698dda37b114c2b8ecf39235c094b6275
wujun728 <wujun728@hotmail.com>
2021-08-23 23:56:42 +0800

    1

--- doc/doc/ssm-gen/src/main/java/cn/edu/scu/virjarjcd/codegertator/IVirjarComponent.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.scu.virjarjcd.codegertator;
-
-public interface IVirjarComponent {
-
-	public boolean hasvalidate();
-	public boolean validate();
-}
--- doc/doc/ssm-gen/src/main/java/cn/edu/scu/virjarjcd/feature/service/ServiceApiGerator.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.scu.virjarjcd.feature.service;
-
-import org.mybatis.generator.codegen.mybatis3.model.BaseRecordGenerator;
-
-public class ServiceApiGerator extends BaseRecordGenerator {
-
-}
