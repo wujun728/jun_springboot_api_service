f3ffbae200bec862d5b9c0991b32ed81637648d3
wujun728 <wujun728@163.com>
2021-10-01 01:29:17 +0800

    1

--- jun_springboot_admin/src/main/resources/static/layui-ext/tableSelect.js
+++ jun_springboot_admin/src/main/resources/static/layui-ext/tableSelect.js
@@ -132,7 +132,7 @@ layui.define(['table', 'jquery', 'form'], function (exports) {
             function defaultChecked (res, curr, count){
                 if(opt.checkedKey && elem.attr('ts-selected')){
                     var selected = elem.attr('ts-selected').split(",");
-                    for(var i=0;i<res.data.records.length;i++){
+                    for(var i=0;i<res.data.length;i++){
                         for(var j=0;j<selected.length;j++){
                             if(res.data[i][opt.checkedKey] == selected[j]){
                                 checkedData.push(res.data[i])
