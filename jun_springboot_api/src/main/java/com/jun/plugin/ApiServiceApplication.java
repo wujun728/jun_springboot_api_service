47f9ee67935f81833893c07b288d43922779d3cf
wujun728 <wujun728@163.com>
2022-02-28 16:40:53 +0800

    1

--- jun_springboot_api/src/main/java/com/jun/plugin/ApiServiceApplication.java
+++ jun_springboot_api/src/main/java/com/jun/plugin/ApiServiceApplication.java
@@ -1,15 +1,35 @@
 package com.jun.plugin;
 
-//import org.mybatis.spring.annotation.MapperScan;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
-import tk.mybatis.spring.annotation.MapperScan;
+import java.net.InetAddress;
 
-@SpringBootApplication
-@MapperScan(basePackages = {"com.jun.plugin.base.mapper","com.jun.plugin.biz.mapper"})
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@MapperScan("com.jun.plugin.**.mapper")
+@Slf4j
+//@ServletComponentScan(basePackages = {"com.cosmoplat.config.filter"}) //这一句完成了配置，Springboot的”懒理念“真的厉害。
 public class ApiServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ApiServiceApplication.class, args);
-	}
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext application = SpringApplication.run(ApiServiceApplication.class, args);
+
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Api Test Json: \thttp://{}:{}/public/json\n\t" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+
 }
--- jun_springboot_api/src/main/java/com/jun/plugin/biz/mapper/CustomerMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jun.plugin.biz.mapper;
-
-import com.jun.plugin.base.core.Mapper;
-import com.jun.plugin.biz.model.Customer;
-
-public interface CustomerMapper extends Mapper<Customer> {
-}
