db9f0a705a468ae7308ceb33e5a7126c579b5ce1
wujun728 <wujun728@163.com>
2022-04-15 20:44:46 +0800

    1

---
+++ src/main/java/com/jun/plugin/system/common/config/MyBatisPlusConfig.java
@@ -0,0 +1,28 @@
+package com.jun.plugin.system.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis plus config
+ *
+ * @author wujun
+ * @version V1.0
+ * @date 2020年3月18日
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    /**
+     * 配置mybatis-plus 分页查件
+     */
+	@Bean
+	public PaginationInterceptor paginationInterceptor() {
+		PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+		// 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求 默认false
+		// paginationInterceptor.setOverflow(false);
+		// 设置最大单页限制数量，默认 500 条，-1 不受限制
+		// paginationInterceptor.setLimit(500);
+		return paginationInterceptor;
+	}
+}
---
+++ src/main/java/com/jun/plugin/system/common/filter/OpenapiConfig.java
@@ -0,0 +1,16 @@
+package com.jun.plugin.system.common.filter;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "openapi")
+@Data
+public class OpenapiConfig {
+ 
+    private List<App> apps;
+}
+
